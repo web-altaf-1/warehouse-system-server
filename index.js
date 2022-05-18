5228260d669328b3504a2fdfb8273390e12b8a53
web-altaf-1 <web.altaf.1@gmail.com>
2022-05-19 00:01:23 +0600

    mongodb was connected

--- index.js
+++ index.js
@@ -12,21 +12,41 @@ app.use(cors());
 app.use(express.json());
 
 
-
-
-
 const uri = "mongodb+srv://dbCars1:4TgpwW4jAHu29MC4@cluster0.cer7g.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("items").collection("car");
-    console.log('Warehouse Database Connected');
-  });
+
+
+async function run() {
+    try {
+        await client.connect();
+        const itemsCollection = client.db("items").collection("car");
+        console.log('Warehouse  Database Connected');
+
+        app.get('/',(req,res)=>{
+            res.send('Warehouse server ready')
+        })
+        
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
+
+
+// client.connect(err => {
+//     const Itemscollection = client.db("items").collection("car");
+//     console.log('Warehouse  Database Connected');
+//   });
+
+// app.get('/items',(req,res)=>{
+//     res.send({Itemscollection});
+// })
 
 
 
-app.get('/',(req,res)=>{
-    res.send('Warehouse server ready')
-})
 
 app.listen(port,()=>{
     console.log('Listening the port', port);
